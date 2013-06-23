2c9a21004b0db345598e9ceda7b5dbd3edf879a5
André Lademann <andre@programmerq.eu>
2013-06-23 20:26:44 +0200

    Update dependencies in cms ruby file.

--- lib/capistrano/typo3/cms.rb
+++ lib/capistrano/typo3/cms.rb
@@ -1,11 +1,20 @@
 require "capistrano/typo3/cms/version"
-require "capistrano/typo3/cms/git"
+
 
 
 Capistrano::Configuration.instance(:must_exist).load do
 
   require 'capistrano/recipes/deploy/scm'
   require 'capistrano/recipes/deploy/strategy'
+	require 'railsless-deploy'
+	require 'capistrano_colors'
+	capistrano_color_matchers = [
+		{ :match => /command finished/,       :color => :hide,      :prio => 10 },
+		{ :match => /executing command/,      :color => :blue,      :prio => 10, :attribute => :underscore },
+		{ :match => /^transaction: commit$/,  :color => :magenta,   :prio => 10, :attribute => :blink },
+		{ :match => /git/,                    :color => :white,     :prio => 20, :attribute => :reverse },
+	]
+	colorize( capistrano_color_matchers )
 
   # =========================================================================
   # These variables may be set in the client capfile if their default values
@@ -19,25 +28,7 @@ Capistrano::Configuration.instance(:must_exist).load do
 
   set(:deploy_to) { "/var/www/#{application}" }
 
-  namespace :deploy do
-    desc <<-DESC
-      Prepares one or more servers for deployment. Before you can use any \
-      of the Capistrano deployment tasks with your project, you will need to \
-      make sure all of your servers have been prepared with `cap deploy:setup'. When \
-      you add a new server to your cluster, you can easily run the setup task \
-      on just that server by specifying the HOSTS environment variable:
+  namespace :typo3 do
 
-        $ cap HOSTS=new.server.com deploy:setup
-
-      It is safe to run this task on servers that have already been set up; it \
-      will not destroy any deployed revisions or data.
-    DESC
-    task :setup, :except => { :no_release => true } do
-      dirs = [deploy_to, releases_path, shared_path].join(' ')
-      run "#{try_sudo} mkdir -p #{releases_path} #{shared_path}"
-      run "#{try_sudo} chown -R #{user}:#{runner_group} #{deploy_to}"
-      sub_dirs = shared_children.map { |d| File.join(shared_path, d) }
-      run "#{try_sudo} chmod 2775 #{sub_dirs.join(' ')}"
-    end
-  end
+	end
 end
--- lib/capistrano/typo3/cms/version.rb
+++ lib/capistrano/typo3/cms/version.rb
@@ -1,13 +1,7 @@
 module Capistrano
   module Typo3
     module Cms
-      VERSION = "0.0.1"
-			class Meme
-		def getVersion
-			"YEAR!"
-    end
-  end
-
-    end
-  end
+      VERSION = "0.0.2"
+		end
+	end
 end
