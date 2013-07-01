574f7a40da8b26a76f9b44663e4e4b2400324a00
André Lademann <andre@programmerq.eu>
2013-07-01 23:45:37 +0200

    RSpec tests will now run from the console and IDE.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,16 @@
+# Bundler
+require 'rubygems'
+require 'bundler/setup'
+
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
 require 'capistrano-spec'
 require 'rspec'
 require 'rspec/autorun'
 
+# Add capistrano-spec matchers and helpers to RSpec
 RSpec.configure do |config|
-
+  config.include Capistrano::Spec::Matchers
+  config.include Capistrano::Spec::Helpers
 end
--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -3,6 +3,12 @@ require File.expand_path(File.dirname(__FILE__) + '/../lib/capistrano/typo3/cms/
 
 describe 'Capistrano TYPO3 CMS module' do
 
+#  before do
+#    @configuration = Capistrano::Configuration.new
+#    @configuration.extend(Capistrano::Spec::ConfigurationExtension)
+#    Typo3.Cms.load_into(@configuration)
+#  end
+
   it 'should have the version number' do
     Capistrano::Typo3::Cms::VERSION.match('^[0-9]?[0-9]\.[0-9]?[0-9]\.[0-9]?[0-9]$');
   end
