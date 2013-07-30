bfeed78ec1dd9bd786abc19fe7729e2997372582
André Lademann <andre@programmerq.eu>
2013-07-30 23:12:25 +0200

    Reduced complexity of loud_into method

---
+++ lib/capistrano/typo3/cms/config.rb
@@ -0,0 +1,45 @@
+# ===================================================================
+# These variables may be set in the client capfile if their default values
+# are not sufficient.
+# ===================================================================
+
+# capistrano
+set :scm, :git
+set :git_enable_submodules, true
+_cset :branch, 'master'
+
+# remote-cache
+set :deploy_via, :remote_cache
+
+# capistrano-typo3-cms
+set :env_development, 'development'
+set :env_staging, 'staging'
+set :env_production, 'production'
+set :env_default, "#{env_staging}"
+
+# capistrano-confirm
+set :confirm_stages, "#{env_production}"
+
+# railsless-deploy
+set :php_bin, 'php'
+
+# capistrano-typo3-cms
+set :dir_source, 'src'
+set :dir_deploy, 'deploy'
+set :dir_htdocs, "#{dir_source}/htdocs"
+set :dir_fileadmin, "#{dir_htdocs}/fileadmin"
+set :dir_uploads, "#{dir_htdocs}/uploads"
+set :dir_vendor, "#{dir_source}/vendor"
+
+set(:deploy_to) { "/var/www/#{application}/" }
+
+
+# set color settings for cli
+log_formatters = [
+    {:match => /command finished/, :color => :hide, :priority => 10},
+    {:match => /executing command/, :color => :blue, :priority => 10, :style => :underscore},
+    {:match => /^transaction: commit$/, :color => :magenta, :priority => 10, :style => :blink},
+    {:match => /git/, :color => :white, :priority => 20, :style => :reverse}
+]
+
+log_formatter(log_formatters)
---
+++ lib/capistrano/typo3/cms/deploy.rb
@@ -0,0 +1,52 @@
+namespace :typo3 do
+  namespace :cms do
+
+    # clear cache
+    desc <<-DESC
+    The task removes all files under typo3temp. After removing the data it creates a\
+      index.html for security reasons and the directory permissions will be checked.\
+      Then all the cache tables will be truncated.\
+      This is the exact same as if you click on the flash button in the backend.\
+
+      $ cap  <enviroment> typo3:cms:cc \\
+
+    DESC
+    task :cc do
+      puts 'delete cache files from typo3temp'
+      run "rm -rf #{dir_htdocs}/typo3temp/*"
+      run "touch #{dir_htdocs}/typo3temp/index.html"
+    end
+  end
+end
+
+
+namespace :deploy do
+
+  desc <<-DESC
+  If you’ve ever forgotten to push your changes to the remote repository before\\
+  trying to make a deploy, you will know that it can be really  frustrating to\\
+  think that everything has gone live when actually it has not.\\
+  This little script will help you avoid this situations.\\
+
+  $ cap deploy:check_changes \\
+
+  DESC
+  task :check_changes do
+    if current_revision == real_revision
+      Capistrano::CLI.ui.say('You don\'t have any changes to deploy')
+
+      agree = Capistrano::CLI.ui.agree('Continue (Yes, [No]) ') do |q|
+        q.default = 'n'
+      end
+
+      exit unless agree
+    else
+      # current_revision depends on current_path
+      reset!(:current_path)
+      reset!(:current_revision)
+      reset!(:real_revision)
+    end
+  end
+end
+
+before "deploy:update_code", "deploy:check_changes"
