a3dcaf1cf9697ec3b30a7cdda7a8b6d3e675b92c
André Lademann <andre@programmerq.eu>
2013-07-31 21:56:01 +0200

    Travis please collect and push code coverage!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,6 @@
 # SimpleCov & Coveralls
-require 'simplecov'
 require 'coveralls'
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-    SimpleCov::Formatter::HTMLFormatter,
-    Coveralls::SimpleCov::Formatter
-]
-Coveralls.wear_merged!
+Coveralls.wear!
 
 # Bundler
 require 'rubygems'
