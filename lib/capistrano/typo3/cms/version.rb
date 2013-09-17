69d95631575fab1f1807ce2a47513a8d1aa1b7cb
André Lademann <vergissberlin@googlemail.com>
2013-09-17 22:46:05 +0200

    Changed version number for gem package and supported TYPO3 CMS

--- lib/capistrano/typo3/cms/version.rb
+++ lib/capistrano/typo3/cms/version.rb
@@ -1,8 +1,8 @@
 module Capistrano
   module Typo3
     module Cms
-      VERSION = '1.0.0'
-      TYPO3_CMS_VERSION = '6.0'
+      VERSION = '0.2.0'
+      TYPO3_CMS_VERSION = '6.1'
     end
   end
 end
